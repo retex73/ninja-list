a5b0fce39190ba8c4037dfe639b0b36a127fea83
Peter Williams <retex.williams@gmail.com>
2021-02-08 22:52:59 +0000

    updated navbar

--- comps/Navbar.js
+++ comps/Navbar.js
@@ -5,11 +5,11 @@ const Navbar = () => {
     return (
         <nav>
             <div className="logo">
-                <Image src="/logo.png" width={128} height={77} />
+                <Image src="/logo.png" alt="site logo" width={128} height={77} />
             </div>
             <Link href="/"><a>Home</a></Link>
             <Link href="/about"><a>About</a></Link>
-            <Link href="/ninjas"><a>Ninja Listing</a></Link>
+            <Link href="/ninjas/"><a>Ninja Listing</a></Link>
         </nav>
     );
 }
