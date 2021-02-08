55a1ddf8723fab085097349cb9685eaeb8019277
Peter Williams <retex.williams@gmail.com>
2021-02-08 22:43:25 +0000

    initial

---
+++ pages/about.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+const About = () => {
+    return (
+        <>
+            <Head>
+                <title>Ninja List | About</title>
+                <meta name="keywords" content="ninjas" />
+            </Head>
+            <div>
+                <h1>About</h1>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vero quia eligendi nobis dolorum quasi aut deserunt sit? Consectetur corrupti at facilis quae ab, corporis autem provident, nostrum nobis sapiente tempora.</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vero quia eligendi nobis dolorum quasi aut deserunt sit? Consectetur corrupti at facilis quae ab, corporis autem provident, nostrum nobis sapiente tempora.</p>
+            </div>
+        </>
+    );
+}
+
+export default About;
